8983f3b11c3e7f58168177d714199a022d807e84
zidanrafidin <zidanrafidin@gmail.com>
2025-08-08 22:13:39 +0700

    Upload perulangan break & continue

---
+++ Perulangan break & continue.py
@@ -0,0 +1,22 @@
+# Perulangan break & continue.py
+# Perulangan break & continue digunakan untuk mengontrol alur perulangan.
+
+# "break" : Berhenti dan keluar dari perulangan.
+
+# contoh perulangan break :
+while True:
+    n = int(input("masukkan nomer habis bagi 2: "))
+    if n % 2 != 0:
+        break
+
+    print("%d habis dibagi 2" % (n))
+
+
+# "continue" : Melewati sisa kode di perulangan saat ini dan langsung lanjut ke perulangan berikutnya.
+
+# contoh perulangan continue
+for i in range(10):
+    if i < 5 or i > 9:
+        continue
+    print("number", i)
+print("selesai")
